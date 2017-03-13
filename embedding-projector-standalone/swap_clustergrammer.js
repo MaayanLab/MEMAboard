9474f19ca6e2f86fbe166ae2fddffa7c63c54f66
anders <abdohlman@gmail.com>
2017-03-13 12:04:17 -0400

    Unstable, got cgrammer in canvas

--- embedding-projector-standalone/swap_clustergrammer.js
+++ embedding-projector-standalone/swap_clustergrammer.js
@@ -4,3 +4,51 @@ console.log("swappin")
 d3.selectAll("canvas").style("display","none")
 // d3.selectAll("canvas").style("display","block")
 
+make_clust('mult_view.json');
+
+var about_string = 'Zoom, scroll, and click buttons to interact with the clustergram. <a href="http://amp.pharm.mssm.edu/clustergrammer/help"> <i class="fa fa-question-circle" aria-hidden="true"></i> </a>';
+
+function make_clust(inst_network){
+
+    d3.json('json/'+inst_network, function(network_data){
+
+      // define arguments object
+      var args = {
+        root: '#scatter',
+        'network_data': network_data,
+        'about':about_string,
+        // 'row_tip_callback':hzome.gene_info,
+        // 'col_tip_callback':test_col_callback,
+        // 'tile_tip_callback':test_tile_callback,
+        // 'dendro_callback':dendro_callback,
+        // 'matrix_update_callback':matrix_update_callback,
+        'sidebar_width':150,
+        // 'ini_view':{'N_row_var':20}
+      };
+
+      resize_container(args);
+
+      d3.select(window).on('resize',function(){
+        resize_container(args);
+        cgm.resize_viz();
+      });
+
+      cgm = Clustergrammer(args);
+
+      // check_setup_enrichr(cgm);
+
+      d3.select(cgm.params.root + ' .wait_message').remove();
+
+  });
+
+}
+
+function resize_container(args){
+
+  var screen_width = window.innerWidth - 100;
+  var screen_height = window.innerHeight - 100;
+
+  d3.select(args.root)
+    .style('width', screen_width+'px')
+    .style('height', screen_height+'px');
+}
