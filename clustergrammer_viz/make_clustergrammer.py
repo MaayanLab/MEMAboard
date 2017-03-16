8225c101b4fc129667287391a47b58933a5e68f3
anders <abdohlman@gmail.com>
2017-03-16 16:10:13 -0400

    Added script for generating tensor-ready data

--- clustergrammer_viz/make_clustergrammer.py
+++ clustergrammer_viz/make_clustergrammer.py
@@ -8,7 +8,9 @@ https://github.com/MaayanLab/clustergrammer-py
 '''
 
 # necessary for scipy hierarchy clustering
+import os
 import sys
+import pandas as pd
 sys.setrecursionlimit(10000)
 
 from clustergrammer import Network
@@ -16,10 +18,20 @@ net = Network()
 
 # load matrix tsv file
 
+DATA_PATH = '../embedding-projector-standalone/oss_data'
 
+# print os.chdir(os.path.join(DATA_PATH, 'MCF10A_SS1_cols.txt'))
 for ss in ['SS1','SS2','SS3']:
-	
-net.load_file('txt/MCF10A_SS1_matrix.txt')
+
+	data = pd.read_table(os.path.join(DATA_PATH, 'MCF10A_'+ss+'.tsv')).values
+	meta = pd.read_table(os.path.join(DATA_PATH, 'MCF10A_'+ss+'_meta.tsv'))
+
+	with open(os.path.join(DATA_PATH, 'MCF10A_'+ss+'_cols.txt')) as f:
+		cols = f.read().splitlines()
+
+	# cols = pd.read_table(os.path.join(DATA_PATH, 'MCF10A_'+ss+'_cols.txt')).values
+
+# net.load_file('txt/MCF10A_SS1_matrix.txt')
 
 # optional filtering and normalization
 ##########################################
@@ -29,7 +41,7 @@ net.load_file('txt/MCF10A_SS1_matrix.txt')
 # net.filter_threshold('row', threshold=3.0, num_occur=4)
 # net.swap_nan_for_zero()
 
-net.make_clust(dist_type='cos', views=[], sim_mat=False)
+# net.make_clust(dist_type='cos', views=[], sim_mat=False)
 
 # write jsons for front-end visualizations
-net.write_json_to_file('viz', 'json/mult_view.json', 'indent')
+# net.write_json_to_file('viz', 'json/mult_view.json', 'indent')
